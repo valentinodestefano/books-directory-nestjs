3b55f39b5c0e67ccfdd8f0dcf84123cec4d11ed0
developerValentino <developer@sinnotaria.cl>
2021-12-14 11:01:08 -0400

    adding get book

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, HttpStatus, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, NotFoundException, Param, Post, Res } from '@nestjs/common';
 
 import { CreateBookDTO } from './dto/book.dto';
 
 import { BookService } from './book.service';
+import { NotFoundError } from 'rxjs';
 
 @Controller('book')
 export class BookController {
@@ -16,6 +17,27 @@ export class BookController {
             message: 'received',
             book: book
         });
+    }
+
+    @Get('/getBooks')
+    async getBooks(@Res() res){
+        const books = await this.bookService.getBooks();
+        return res.status(HttpStatus.OK).json({
+            message: 'Succesfull',
+            books: books
+        })
+    }
+
+    @Get('/:bookID')
+    async getBook(@Res() res, @Param('bookID') bookID: string){
+        const book = await this.bookService.getBook(bookID);
+        if (!book) {
+           return res.status(HttpStatus.BAD_REQUEST);
+        }
+        return res.status(HttpStatus.OK).json({
+            message: 'succesfull',
+            book: book
+        })
     }
 
 }
--- src/book/dto/book.dto.ts
+++ src/book/dto/book.dto.ts
@@ -1,8 +1,39 @@
+import { ApiModelProperty } from '@nestjs/swagger/dist/decorators/api-model-property.decorator';
 export class CreateBookDTO {
+
+    @ApiModelProperty({
+        description: 'Book Name',
+        example: 'Rich Dad Poor Dad',
+    })
     readonly name: string;
+
+    @ApiModelProperty({
+        description: 'Publication year of the book',
+        example: '2021',
+    })
     readonly yearOfPublication: number;
+
+    @ApiModelProperty({
+        description: 'Description of the book',
+        example: 'This book explains the value of the money...',
+    })
     readonly description: string;
+
+    @ApiModelProperty({
+        description: 'Date when is created the book',
+        example: '2021-12-12',
+    })
     readonly createAt: Date;
+
+    @ApiModelProperty({
+        description: 'image of the book',
+        example: 'URL',
+    })
     readonly imageURL: string;
+
+    @ApiModelProperty({
+        description: 'Book Status',
+        example: 'not read',
+    })
     readonly status: string;
 }
